335dd364c64cd3756a254b625b9e530ca60a49d8
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-09-18 16:50:50 +0530

    debug

--- scripts/broken-links.sh
+++ scripts/broken-links.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+# set -e
+# ./scripts/build-pages.sh
+
+# echo "Allowing time to build the site..."
+# for i in {1..20}; do
+#     echo "Waiting... ($i/20)"
+#     sleep 1
+# done
+# echo "Finished checking"
+
+# docker run --rm --network biothermica-site-2026 dcycle/broken-link-checker:3 http://jekyll:4000
+
+#!/bin/bash
 set -e
+
 ./scripts/build-pages.sh
 
-echo "Allowing time to build the site..."
-for i in {1..20}; do
-    echo "Waiting... ($i/20)"
-    sleep 1
-done
-echo "Finished checking"
+echo "Checking broken links..."
 
-docker run --rm --network biothermica-site-2026 dcycle/broken-link-checker:3 http://jekyll:4000
+docker run --rm \
+  --network biothermica-site-2026 \
+  dcycle/broken-link-checker:3 \
+  http://jekyll:4000
